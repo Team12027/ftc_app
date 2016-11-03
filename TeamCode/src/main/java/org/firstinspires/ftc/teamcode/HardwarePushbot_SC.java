d9192eca425b5fc4516f7c5db169aa254df4a63a
Michelle Li <michellecarolynclark@gmail.com>
2016-11-02 20:22:50 -0700

    increased arm rotation power to 90%

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot_SC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot_SC.java
@@ -31,8 +31,8 @@ public class HardwarePushbot_SC
     public Servo    rightClaw   = null;
 
     public static final double MID_SERVO       =  0.5 ;
-    public static final double ARM_UP_POWER    =  0.45 ;
-    public static final double ARM_DOWN_POWER  = -0.45 ;
+    public static final double ARM_UP_POWER    =  0.9 ;
+    public static final double ARM_DOWN_POWER  = -0.9 ;
 
     /* local OpMode members. */
     HardwareMap hwMap           =  null;
